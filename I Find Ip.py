096f70c02cc5d925fc4834e1656cd4c153e6eaf6
Dr-FrosKa <53023567+Dr-FrosKa@users.noreply.github.com>
2019-07-17 19:34:06 -0500

    Update I Find Ip.py

--- I Find Ip.py
+++ I Find Ip.py
@@ -1,5 +1,12 @@
+# pip install pyfiglet
+import pyfiglet
 import socket
+
+ascii_banner = pyfiglet.figlet_format("I Find Ip")
+print ("Dr.Fr0sKa")
+print(ascii_banner)
+
 if __name__=='__main__':
     hostname = raw_input ("Enter The Target :")
     addr = socket.gethostbyname(hostname)
-    print("The Ip {} address is  {}   ".format(hostname,addr))
+print("The Ip {} address is  {}   ".format(hostname,addr))
