e77fa799e312204e0da92e6c848e50ba5ebffbf7
Dr-FrosKa <53023567+Dr-FrosKa@users.noreply.github.com>
2019-07-17 19:55:22 -0500

    Update I Find Ip.py

--- I Find Ip.py
+++ I Find Ip.py
@@ -1,12 +1,17 @@
-# pip install pyfiglet
+import terminal_banner
 import pyfiglet
 import socket
+def cls(): 
+    print('\n'* 100)
+cls()    
+banner_text = "Dr.Fr0sKa.\n\nSnap:Faris_th7 | Instagram:4o16."
+my_banner = terminal_banner.Banner(banner_text)
+print(my_banner)
 
 ascii_banner = pyfiglet.figlet_format("I Find Ip")
-print ("Dr.Fr0sKa")
 print(ascii_banner)
 
 if __name__=='__main__':
     hostname = raw_input ("Enter The Target :")
     addr = socket.gethostbyname(hostname)
-print("The Ip {} address is  {}   ".format(hostname,addr))
+print("The Ip  {}  address is || {} ||  ".format(hostname,addr))
